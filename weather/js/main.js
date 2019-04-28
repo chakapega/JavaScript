58ace88742ae8038ee72a4391cc3f83695699218
Vadim <chakapega@gmail.com>
2019-04-28 16:49:36 +0300

    changed getWeather and create showResults func..s

--- weather/js/main.js
+++ weather/js/main.js
@@ -1,24 +1,51 @@
 const URL = 'http://api.apixu.com/v1/current.json?key=c9bf02c8667a4e998ad205818192304&q=';
 const choiceCityForm = document.querySelector('.choice-city-form');
 const buttonShowWeather = document.querySelector('.choice-city-form__button');
+let city;
+let allDataWeather;
 
-const getWeather = (city) => {
+const getWeather = () => {
 
   const req = new XMLHttpRequest();
   req.open('GET', `${URL}` + city, false);
   req.send();
-  console.log(JSON.parse(req.response));
+  
+  allDataWeather = JSON.parse(req.response);
+  console.log(allDataWeather);
+  
+};
 
+const showResults = () => {
+  const conditionImg = document.querySelector('.condition__img');
+  conditionImg.src = 'http:' + allDataWeather.current.condition.icon;
+
+  const conditionText = document.querySelector('.condition__text');
+  conditionText.textContent = allDataWeather.current.condition.text;
+
+  const weatherDataWind = document.querySelector('.weather-data__wind');
+  weatherDataWind.textContent = 'Wind: ' + allDataWeather.current.wind_kph + ' km/h';
+
+  const weatherDataPrecipitation = document.querySelector('.weather-data__precipitation');
+  weatherDataPrecipitation.textContent = 'Precipitation: ' + allDataWeather.current.precip_mm + ' mm';
+
+  const weatherDataPressure = document.querySelector('.weather-data__pressure');
+  weatherDataPressure.textContent = 'Pressure: ' + allDataWeather.current.pressure_mb + ' mb';
+
+  const weatherDataTemperature = document.querySelector('.weather-data__temperature');
+  weatherDataTemperature.textContent = 'Temperature: ' + allDataWeather.current.temp_c + ' °c';
 };
 
 choiceCityForm.addEventListener('submit', e => {
   
   e.preventDefault();
 
-  const city = e.target['choice-city-form__input'].value;
+  city = e.target['choice-city-form__input'].value;
   
-  getWeather(city);
+  getWeather();
+  showResults();
   
 });
+
+
 
 
