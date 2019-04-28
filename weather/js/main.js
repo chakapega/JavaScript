fab4832f0e589f8981e7147810f003be48cefb56
Vadim <chakapega@gmail.com>
2019-04-28 03:50:25 +0300

    added js code

--- weather/js/main.js
+++ weather/js/main.js
@@ -1,15 +1,24 @@
+const URL = 'http://api.apixu.com/v1/current.json?key=c9bf02c8667a4e998ad205818192304&q=';
+const choiceCityForm = document.querySelector('.choice-city-form');
+const buttonShowWeather = document.querySelector('.choice-city-form__button');
 
-function getWeather() {
+const getWeather = (city) => {
+
   const req = new XMLHttpRequest();
-  req.open('GET', 'http://api.apixu.com/v1/current.json?key=c9bf02c8667a4e998ad205818192304&q=Minsk', false);
+  req.open('GET', `${URL}` + city, false);
   req.send();
-
   console.log(JSON.parse(req.response));
+
 };
 
-getWeather();
+choiceCityForm.addEventListener('submit', e => {
+  
+  e.preventDefault();
+
+  const city = e.target['choice-city-form__input'].value;
+  
+  getWeather(city);
+  
+});
 
 
-
-
-// 93c57d868bafc0824b515f56873c85c4
